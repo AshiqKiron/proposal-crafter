e6d691006ec13c4c1f3cd899a3f596c775906949
Ashiquzzaman Kiron <ashiq.kiron@gmail.com>
2026-05-05 12:03:23 +0600

    Delete blocks directory

--- blocks/sender-email/render.php
+++
@@ -1,9 +0,0 @@
-<?php 
-defined( 'ABSPATH' ) || exit;
-proposal_crafter_block_start( $attributes );
-?>
-    <p class="pc-sender-email">
-        <?php echo esc_html( $attributes['content'] ?? '' ); ?>
-    </p>
-<?php 
-proposal_crafter_block_end();
